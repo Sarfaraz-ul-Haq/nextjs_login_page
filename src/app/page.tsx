ec848d9d63979f1556b927a996c6310bb0d879f9
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-05-12 00:08:07 +0500

    add text sign up now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ function Home() {
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-800">
       <div className="flex flex-col gap-3 border border-gray-900  bg-gray-700 px-24 py-24">
-        <div className="font-bold text-3xl text-white flex justify-center mb-10">
+        <div className="font-bold text-3xl text-gray-300 flex justify-center mb-10">
           LOGIN
         </div>
         <div>
@@ -30,7 +30,7 @@ function Home() {
             value={email}
             placeholder="Email"
             onChange={onEmailChange}
-            style="focus:outline-none hover:border-green-700 hover:border-2 hover:shadow-green-glow rounded px-2 py-2 transition duration-300 ease-in-out bg-gray-600 border border-gray-500 py-2"
+            style="focus:outline-none hover:border-green-700 hover:border-2 hover:shadow-green-glow rounded px-7 py-2 transition duration-300 ease-in-out bg-gray-600 border border-gray-500 py-2"
           />
         </div>
         <div>
@@ -40,14 +40,18 @@ function Home() {
             value={password}
             placeholder="Password"
             onChange={onPasswordChange}
-            style="focus:outline-none hover:border-green-700 hover:border-2 hover:shadow-green-glow rounded px-2 py-2 transition duration-300 ease-in-out bg-gray-600 border border-gray-500 py-2"
+            style="focus:outline-none hover:border-green-700 hover:border-2 hover:shadow-green-glow rounded px-7 py-2 transition duration-300 ease-in-out bg-gray-600 border border-gray-500 py-2"
           />
         </div>
         <div className="flex justify-center">
           <Button
             text="LOGIN"
-            style="border rounded border-gray-600 bg-gray-600 px-24 py-2"
+            style="border rounded border-gray-600 bg-gray-600 px-28 py-2 font-bold text-gray-300"
           />
+        </div>
+        <div className="flex justify-center">
+          <div>Not a member?</div>
+          <Button text="Signup now" style="font-bold text-green-800 ml-3" />
         </div>
       </div>
     </div>
